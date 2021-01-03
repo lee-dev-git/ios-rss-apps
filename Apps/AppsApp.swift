82d2fc53e0b33e65e2344d12fa2057dab7aced92
lee-dev-git <lee-dev@kakao.com>
2021-01-03 19:48:06 +0900

    Initial Commit

---
+++ Apps/AppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppsApp.swift
+//  Apps
+//
+//  Created by 이지원 on 2021/01/03.
+//
+
+import SwiftUI
+
+@main
+struct AppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
